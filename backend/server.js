ca938ad617ef297ed6177c1cf1e08b5eae4a5f01
Alhaadii <alcazaam1233@gmail.com>
2025-05-29 04:40:09 -0700

    Database Co;nnection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 import express from "express";
 
 import dotenv from "dotenv";
+import { connectDB } from "./config/database.js";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
 
-
-app.get("/api/books", (req, res) => {
-
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend server!");
+});
