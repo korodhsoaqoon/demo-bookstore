eab5193de77c7a8ac15ba12f369ea0b1cbe215df
Alhaadii <alcazaam1233@gmail.com>
2025-06-01 13:38:22 -0700

    change mongodb url

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_SERVER_UR);
+    await mongoose.connect(process.env.MONGO_SERVER_URL);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
