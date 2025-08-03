02edd2285d7fe846933b87d1c9d7f6f13925bf30
Abdullahi <hajiabdi213@gmail.com>
2025-08-03 08:21:46 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -10,20 +10,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/books", bookRoute);
+
 const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("/*", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+// }
 
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
-
-app.use("/api/books", bookRoute);
